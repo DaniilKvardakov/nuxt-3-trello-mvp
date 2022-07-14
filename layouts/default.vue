e9404273a8ab8930cd050644656ada9f593659c7
Daniil Kvardakov <hopeincredible1@gmail.com>
2022-07-14 20:50:59 +0700

    feature: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Default Layout</p>
+    <nav>
+        <nuxt-link to="/">Home</nuxt-link>    
+    </nav>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
